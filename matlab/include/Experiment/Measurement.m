3e0f6d753c6c49ed497fae476b9a943ca075f397
Bill Fan <fanbenlong@gmail.com>
2022-04-16 03:44:57 -0400

    Matlab: Added experiment and tag plotting

--- matlab/include/Experiment/Measurement.m
+++ matlab/include/Experiment/Measurement.m
@@ -1,9 +1,11 @@
-classdef Measurement
+classdef Measurement < handle & matlab.mixin.Copyable
     %CAPTURE A "Measurement" is a single "experiment measurement" - a capturing
     %of the poses of Apriltags attached to a manipulator inflated to a
     %certain pressure.
     
     properties
+        group
+
         bag_path % Store the path to the rosbag and retrieve as needed
 
         % Experiment Setup Information?
@@ -11,6 +13,8 @@ classdef Measurement
 
         tags_meas
         tags_fit
+        g_tag_offset
+        f_transform_tags
 
         arm_obj
         f_contraction_model = @contraction_fit;
@@ -18,7 +22,11 @@ classdef Measurement
         % ??
         h_o_fit
         h_o_model
-        group
+
+        % Plotting parameters
+        gh_tags_meas
+        gh_tags_fit
+
     end
     
     methods
@@ -30,13 +38,19 @@ classdef Measurement
                 tags Tag
                 arm_obj Arm
                 options.group = SE3()
+                options.g_tag_offset = [];
             end
             % Create Measurement object and extract tag poses
             obj.bag_path = bag_path;
             obj.v_pressure = v_pressure(:);
-            obj.tags_meas = tags;
+            obj.tags_meas = copy(tags);
             obj.arm_obj = arm_obj;
             obj.group = options.group;
+            if isempty(options.g_tag_offset)
+                obj.g_tag_offset = eye(obj.group.mat_size);
+            else
+                obj.g_tag_offset = options.g_tag_offset;
+            end
 
             bag_obj = rosbag(bag_path);
             
@@ -59,7 +73,8 @@ classdef Measurement
                 R = quat2rotm(tform_i.Transform.Rotation.readQuaternion());
                 % Apply extra 90 deg ccw rotation to correct for misplaced tags
                 % TODO: Re-evaluate?
-                R = R * eul2rotm([pi/2, 0, 0], 'zyx');
+                % TODO: Moving this to a more universal approach
+                % R = R * eul2rotm([pi/2, 0, 0], 'zyx');
 
                 T_obj = tform_i.Transform.Translation;
                 T = [T_obj.X; T_obj.Y; T_obj.Z];
@@ -80,16 +95,29 @@ classdef Measurement
                 end
             end
 
+            % We want to "align/attach" the measured tags to the oriented 
+            % arm object. Specifically, the base_tag needs to be on the
+            % base muscle. We want to find the *left* transformation that
+            % does this, so we can transform *all* of the tags
+            % 
+            % g_transform * g_base_tag = g_0_i * g_offset
+            % => g_transform = g_0_i * g_offset * inv(g_base_tag)
+            % where i is the base_tag's id. This aligns the base_tag to be
+            % "attached to the muscle" in the world frame.
+            g_alignment_transform = ...
+                arm_obj.muscles(base_tag.muscle_id).g_0 * obj.g_tag_offset * inv(base_tag.g_pose);
+
+            obj.tags_meas.apply_transform(@(g_tag) g_alignment_transform * g_tag);
+
             [obj, obj.tags_fit] = obj.fit_curve();
         end
         
-        function [obj, tags_fit] = fit_curve(obj, options)
+        function [obj, tags_fit] = fit_curve(obj)
             arguments
                 obj
-                options.offsets = repmat({eye(obj.group.mat_size)}, 1, length(obj.tags_meas));
             end
             tag_num = length(obj.tags_meas);
-            tags_fit = obj.tags_meas;
+            tags_fit = copy(obj.tags_meas);
             
             % Build cost function for fitting the curve
             function [cost, cell_g_tags] = f_cost(h_o)
@@ -108,20 +136,27 @@ classdef Measurement
                 for i = 1 : tag_num
                     % Lie algebra distance between curve fit and measured position
                     % Right subtraction
+
+                    % First let's get the transformation from the
+                    % base-curve to the muscle the tag belongs to
                     g_o_Xi = inv(obj.arm_obj.muscle_o.g_0) * obj.arm_obj.muscles(obj.tags_meas(i).muscle_id).g_0;
-                    % TODO: handle offsets
-                    g_o_tag = g_o_Xi * options.offsets{i}; % Apply any offsets that may be needed
+                    g_o_tag = g_o_Xi * obj.g_tag_offset; % Apply additional tag-frame constant offset (such as mounting height)
                     
-                    % g_meas * g_error = g_tag(h_o)
-                    % g_error = inv(g_meas) * g_tag(h_o)
+                    % g_tag = g_0 * expm(h_o * t) * g_o_tag: Transform to
+                    % arm base, flow along base curve for given amount of
+                    % distance, and then transform from base curve to tag
+                    % pose
                     g_tag = obj.arm_obj.muscle_o.g_0 * obj.group.algebra.expm(h_o * obj.tags_meas(i).t) * g_o_tag;
-                    SE3_error = inv(obj.tags_meas(i).g_pose) * g_tag;
+
+                    % Calculate error vector
+                    SEn_error = inv(obj.tags_meas(i).g_pose) * g_tag;
                     
                     % Lie-algebraic (R^6) form of the error vector.
-                    v_se3_error = obj.group.algebra.vee(logm(SE3_error));
+                    v_se3_error = obj.group.algebra.vee(logm(SEn_error));
                     
+                    % Compute quardatic cost
                     cost_i = v_se3_error' * K * v_se3_error;
-                    cost = cost + cost_i;
+                    cost = cost + cost_i; % Running sum of cost
                     
                     % Don't store tag_poses if called by fminsearch
                     if nargout > 1
@@ -135,7 +170,7 @@ classdef Measurement
                 elseif obj.group.dof == 6
                     mat_shear_cost = diag([0, 1, 1, 0, 0, 0]);
                 end
-                cost = cost + h_o' * mat_shear_cost * h_o;
+%                 cost = cost + h_o' * mat_shear_cost * h_o;
             end
         
             % Calculate initial value: The h_o vector predicted by the
@@ -145,10 +180,59 @@ classdef Measurement
 
             % Perform optimization
             [obj.h_o_fit, ~] = fminsearch(@f_cost, obj.h_o_model);
-            
             [~, cell_g_tags_fit] = f_cost(obj.h_o_fit);
             tags_fit = tags_fit.set_pose_all(cell_g_tags_fit);
             obj.tags_fit = tags_fit;
+        end
+
+        function plot_measurement(obj, ax)
+            grid on
+            axis equal
+
+            %%% Plot the arms
+            % Initialize plotting
+            model_arm = copy(obj.arm_obj);
+            fit_arm = copy(obj.arm_obj);
+            
+            % Set plotting parameters
+            line_options_muscles = struct("LineWidth", 3);
+            line_options_spacers = struct("LineWidth", 2);
+            
+            f_initialize_plotting = @(obj) ...
+                obj.initialize_plotting(...
+                    ax, ...
+                    "line_options_muscles", line_options_muscles, ...
+                    "line_options_spacers", line_options_spacers);
+
+            f_initialize_plotting(model_arm);
+            f_initialize_plotting(fit_arm);
+            
+            % Darken plotting colors in model arm
+            for i = 1 : length(model_arm.muscles)
+                color_current = model_arm.muscles(i).color;
+                model_arm.muscles(i).color = hsv2rgb(rgb2hsv(color_current) .* [1, 1, 0.75]);
+                model_arm.muscles(i).lh.LineStyle = ":";
+            end
+
+            % Make model arm spacers also dashed
+            for i = 1 : length(model_arm.v_lh_spacers)
+                model_arm.v_lh_spacers(i).LineStyle = ":";
+            end
+            
+            % Bring solid line (Fitted muscle curves) to the back
+            for i = 1 : length(fit_arm.muscles)
+                uistack(fit_arm.muscles(i).lh, "bottom");
+            end
+
+            % Actually plot the arms now
+            v_l = contraction_fit_poly(obj.v_pressure);
+            model_arm.update_arm(v_l, obj.h_o_model);
+            %fit_arm.update_arm(v_l, h_o_fit .* [1 0 0 1 1 1]'); % Ignore shearing
+            fit_arm.update_arm(v_l, obj.h_o_fit);
+
+            % Plot the tags
+            obj.gh_tags_meas = obj.tags_meas.plot_tags(ax);
+            obj.gh_tags_fit = obj.tags_fit.plot_tags(ax, "green");
         end
     end
 end
---
+++ matlab/include/Experiment/Tag.m
@@ -0,0 +1,117 @@
+classdef Tag < handle & matlab.mixin.Copyable
+    %TAG Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        group = SE3()
+
+        id
+        g_pose
+        muscle_id % id of the muscle the tag is attached to
+        t % Percent position along the arm
+    end
+    
+    methods
+        function obj = Tag(v_id, cell_g_poses, v_muscle_ids, v_t, options)
+            arguments
+                v_id = 0;
+                cell_g_poses = {} % Signals its a default value to be initialized later
+                v_muscle_ids = zeros(size(v_id));
+                v_t = zeros(size(v_id));
+                options.group = SE3();
+            end
+
+            if isempty(cell_g_poses)
+                cell_g_poses = repmat({eye(options.group.mat_size)}, size(v_id));
+            end
+
+            % TODO: Validate all input sizes agree
+            [m, n] = size(v_id);
+
+            obj(m, n) = copy(obj); % Preallocate array of objects
+            for i = 1 : m
+                for j = 1 : n
+                    obj(i, j).id = v_id(i, j);
+                    obj(i, j).g_pose = cell_g_poses{i, j};
+                    obj(i, j).muscle_id = v_muscle_ids(i, j);
+                    obj(i, j).t = v_t(i, j);
+                    obj(i, j).group = options.group;
+                end
+            end
+        end
+
+        function obj = apply_transform(obj, f_transform)
+            [m, n] = size(obj);
+            for i = 1 : m
+                for j = 1 : n
+                    obj(i, j).g_pose = f_transform(obj(i, j).g_pose);
+                end
+            end
+        end
+
+        function obj = set_pose_all(obj, cell_g_poses)
+            % TODO: Validate all input sizes agree
+            [m, n] = size(obj);
+            for i = 1 : m
+                for j = 1 : n
+                    obj(i, j).g_pose = cell_g_poses{i, j};
+                end
+            end
+        end
+
+        function [obj, gh_tforms] = plot_tags(obj, ax, color, size, mesh_file)
+            % Applies plotTransform() TF frame plotting to a cell-array
+            %of SE3 pose matrices
+            
+            arguments
+                obj Tag
+                ax = gca()
+                color = "red";
+                size = 0.03;
+                mesh_file = "tf_frame.stl";
+            end
+            
+            if isa(obj(1).group, "SE2")
+                % SE2
+                f_cellfun_t = @(mat) [mat(1:2, 3); 0];
+                f_cellfun_quat = @(mat) transpose(rotm2quat(blkdiag(mat(1:2, 1:2), 1)));
+            elseif isa(obj(1).group, "SE3")
+                % SE3
+                f_cellfun_t = @(mat) mat(1:3, 4);
+                f_cellfun_quat = @(mat) transpose(rotm2quat(mat(1:3, 1:3)));
+            else
+                error("Unrecognized group value for Tag object")
+            end
+
+            g_tforms = {obj.g_pose};
+
+            f_extract_with_cellfun = @(f_cellfun) ...
+                cell2mat(cellfun(f_cellfun, g_tforms, 'uniformoutput', false));
+            
+            % Apply "extraction" functions to the cell array
+            t_tforms = transpose(f_extract_with_cellfun(f_cellfun_t));
+            quat_tforms = transpose(f_extract_with_cellfun(f_cellfun_quat));
+            
+            % Set plotTransforms Parent options to the specified Axis
+            plot_options = struct();
+            plot_options.MeshFilePath = mesh_file;
+            plot_options.framesize = size;
+            plot_options.MeshColor = color;
+            plot_options.parent = ax;
+            
+            plotTransforms(t_tforms, quat_tforms, plot_options);
+            
+            % Newest axis children (corresponding to the plotTransform) is pre-pended
+            % to Axis.Children so we can grab the Group Handle (gh).
+            gh_tforms = ax.Children(1);
+            
+            if isa(obj(1).group, "SE2")
+                view(ax, [0, 90]);
+            end
+
+            grid on
+            axis equal
+        end
+    end
+end
+
